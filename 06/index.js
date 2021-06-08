94ef16355b6e3ba3189a4f5152b19c4790e43590
kkata <14849535+kkata@users.noreply.github.com>
2021-06-08 18:30:06 +0900

    finished 06

---
+++ 06/index.js
@@ -0,0 +1,21 @@
+const ul = document.querySelector("ul");
+
+const array = [
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+];
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(array);
+  }, 3000);
+});
+
+promise.then((value) => {
+  value.forEach((element, index) => {
+    ul.insertAdjacentHTML(
+      "beforeend",
+      `<li><a href="/${element.to}"><img src="${element.img}" alt="${element.alt}">${element.text}</a></li>`
+    );
+  });
+});
