a9504d3d9706d1db936bf9a9396c88053be71573
kkata <14849535+kkata@users.noreply.github.com>
2021-06-08 11:32:26 +0900

    finished 04

---
+++ 04/index.js
@@ -0,0 +1,13 @@
+const ul = document.querySelector("ul");
+
+const array = [
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+];
+
+array.forEach((element, index) => {
+  ul.insertAdjacentHTML(
+    "beforeend",
+    `<li><a href="/${element.to}"><img src="${element.img}" alt="${element.alt}">${element.text}</a></li>`
+  );
+});
