7fbf16b91a429c011f6922a5bc495430dd289fd1
kkata <14849535+kkata@users.noreply.github.com>
2021-06-10 15:52:47 +0900

    finished 14

---
+++ 14/index.js
@@ -0,0 +1,58 @@
+const ul = document.querySelector("ul");
+const loadingImg = document.createElement("img");
+loadingImg.src = "../image/loading-circle-1.gif";
+const buttonShow = document.getElementById("button-show");
+const modal = document.getElementById("modal");
+const modalInner = document.getElementById("modal-inner");
+const buttonModal = document.getElementById("button-modal");
+const buttonClose = document.getElementById("button-close");
+const inputNumber = document.getElementById("input-number");
+
+function show() {
+  modalInner.appendChild(loadingImg);
+  console.log(inputNumber.value);
+  fetch("https://jsondata.okiba.me/v1/json/49ggL210609132113")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(err.message);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      modalInner.removeChild(loadingImg);
+      modalInner.removeChild(buttonShow);
+      data.data.forEach((element, index) => {
+        ul.insertAdjacentHTML(
+          "beforeend",
+          `<li><a href="/${element.to}"><img src="${element.img}" alt="${element.alt}">${element.text}</a></li>`
+        );
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+buttonShow.addEventListener(
+  "click",
+  (event) => {
+    show();
+  },
+  false
+);
+
+buttonModal.addEventListener(
+  "click",
+  (event) => {
+    modal.style.display = "block";
+  },
+  false
+);
+buttonClose.addEventListener(
+  "click",
+  (event) => {
+    modal.style.display = "none";
+  },
+  false
+);
